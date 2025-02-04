d106f1d2bef0d43591ab1c3c6893e94ccf9ed1bf
PaulMahar <paulmahar@outlook.com>
2025-02-04 17:35:59 -0400

    Completed

--- app/src/main/java/com/example/gameplan/HomeSkeleton.kt
+++ app/src/main/java/com/example/gameplan/HomeSkeleton.kt
@@ -1,38 +1,20 @@
 package com.example.gameplan
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class TandC : AppCompatActivity() {
+class HomeSkeleton : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tand_c)
+        setContentView(R.layout.activity_home_skeleton)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val proceedButton = findViewById<ImageButton>(R.id.button)
-
-        proceedButton.setOnClickListener {
-            proceedButton.setImageResource(R.drawable.hover1)
-            val intent = Intent(this, HomeSkeleton::class.java)
-            startActivity(intent)
-
-            Handler(Looper.getMainLooper()).postDelayed({
-                proceedButton.setImageResource(R.drawable.default1)
-            }, 500)
-        }
-
-
-
     }
 }
