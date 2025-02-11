d785df5621a7164c03b0ab6ab12ccf0b35939e69
Nathan M <w0429133@nscc.ca>
2025-02-10 21:31:16 -0400

    Nathan did all of this work by himself and deserves a gold star

--- 2.0/app/src/main/java/com/example/gameplan/ui/theme/AppComponents.kt
+++ 2.0/app/src/main/java/com/example/gameplan/ui/theme/AppComponents.kt
@@ -1,2 +1,64 @@
 package com.example.gameplan.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gameplan.ui.theme.screens.Game
+
+@Composable
+fun ShowGame(game: Game){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center){
+            ElevatedCard(
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),
+                modifier = Modifier
+                    .size(width = 240.dp, height = 100.dp)){
+                Text(
+                    text = game.name,
+                    modifier = Modifier
+                        .padding(16.dp),
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = game.age.toString(),
+                    modifier = Modifier
+                        .padding(top = 6.dp,start = 16.dp),
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+        Spacer(
+            modifier = Modifier
+                .height(16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ShowGamePreview(){
+    ShowGame(game = Game("Minecraft", 2012))
+}
+
+// meow
--- 2.0/app/src/main/java/com/example/gameplan/ui/theme/screens/WelcomeScreen.kt
+++ 2.0/app/src/main/java/com/example/gameplan/ui/theme/screens/WelcomeScreen.kt
@@ -1,17 +1,40 @@
 package com.example.gameplan.ui.theme.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 @Composable fun WelcomeScreen(navController: NavHostController){
     Surface(
-        modifier = Modifier.fillMaxSize().clickable{navController.navigate(Routes.PLAYER_SELECT_SCREEN)}
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable{navController.navigate(Routes.GAMES_SCREEN)}
     ){
-        Column(modifier = Modifier.fillMaxSize().padding(18.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(18.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Hello World",
+                modifier = Modifier.padding(16.dp),
+                textAlign = TextAlign.Center
+            )
         }
     }
+}
+
+@Preview
+@Composable fun WelcomeScreenPreview(){
+    WelcomeScreen(rememberNavController())
 }
